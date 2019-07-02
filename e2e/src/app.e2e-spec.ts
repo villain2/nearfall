81d369b03a8849a881240c0593cc0dae705613af
K.C. Hunter <djvillain@hotmail.com>
2019-07-02 04:13:02 -0400

    updating e2e test.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,13 +10,13 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to angularapp!');
+    expect(page.getParagraphText()).toEqual('Welcome to nearfall!');
   });
 
   it('Should display welcome to angularapp', (done: DoneFn) => {
     return browser.waitForAngularEnabled(true)
       .then(() => browser.sleep(2000))
-      .then(() => expect(page.getParagraphText()).toEqual('Welcome to angularapp!'))
+      .then(() => expect(page.getParagraphText()).toEqual('Welcome to nearfall!'))
       .then(done)
       .catch((err: Error) => done.fail(err));
   });
